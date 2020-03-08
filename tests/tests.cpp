6bdbc42c7c33d3f776d6b53c1cc007c55cbafb35
Petko Georgiev <petko.vas.georgiev@gmail.com>
2020-03-08 13:02:08 +0200

    Add file reading tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -12,10 +12,12 @@ class TestObject : public QObject
 	Q_OBJECT
 
 private slots:
-	void testDeletion();
+	void testReading();
+	void testDeleting();
 
 private:
-	void testDeleteBytes(const QByteArray &data, const QVector<int> &indicesToDelete);
+	void testReadingHelper(const QByteArray &data, const QVector<int> &indicesToRead);
+	void testDeletingHelper(const QByteArray &data, const QVector<int> &indicesToDelete);
 
 	template <typename R, typename C>
 	C createContainer(int size, std::function<R(int)> func)
@@ -39,37 +41,84 @@ private:
 	}
 };
 
-void TestObject::testDeletion() {
+void TestObject::testReading()
+{
+	// Basic read test
+	testReadingHelper("1234567890", {9, 2, 5, 0});
+
+	// Test with bigger file and 'random' reads
+	testReadingHelper(createByteArray(10'000, [](int i) { return i; }),
+					createVector<int>(100, [](int i) { return (i * 11 + 71) % 10'000; }));
+
+	// Test with a very large file and 'random' reads
+	testReadingHelper(createByteArray(1'000'000, [](int i) { return i * 7 + 5; }),
+					createVector<int>(10'000, [](int i) { return (i * 13 + 1) % 1'000'000; }));
+
+	// Read the first half of a file
+	testReadingHelper(createByteArray(100'000, [](int i) { return i * 11 + 5; }),
+					QVector<int>(50'000, 0));
+
+	// Read the second half of a file
+	testReadingHelper(createByteArray(100'000, [](int i) { return i * 11 + 5; }),
+					QVector<int>(50'000, 100'000 - 1));
+
+	// Read 'random' bytes near the begining and near the end of the file
+	testReadingHelper(createByteArray(1'000'000, [](int i) { return i * 9 + 3; }),
+					createVector<int>(10'000, [](int i) { return (i * 13 + 1) % 10'000; }) +
+					createVector<int>(10'000, [](int i) { return 960'000 + (i * 13 + 1) % 10'000; }));
+
+	// Read 'random' bytes in the middle of the file
+	testReadingHelper(createByteArray(1'000'000, [](int i) { return i * 17 + 3; }),
+					createVector<int>(10'000, [](int i) { return 500'000 + (i * 23 + 13) % 10'000; }));
+}
+
+void TestObject::testDeleting() {
 	// Basic delete test
-	testDeleteBytes("abcdef", {1, 3});
+	testDeletingHelper("abcdef", {1, 3});
 
 	// Test with a bigger file and 'random' deletions
-	testDeleteBytes(createByteArray(10'000, [](int i) { return i; }),
+	testDeletingHelper(createByteArray(10'000, [](int i) { return i; }),
 					createVector<int>(100, [](int i) { return (i * 11 + 71) % (10'000 - i); }));
 
 	// Test with a very large file and 'random' deletions
-	testDeleteBytes(createByteArray(1'000'000, [](int i) { return i * 7 + 5; }),
+	testDeletingHelper(createByteArray(1'000'000, [](int i) { return i * 7 + 5; }),
 					createVector<int>(10'000, [](int i) { return (i * 13 + 1) % (1'000'000 - i); }));
 
 	// Delete the first half of a file
-	testDeleteBytes(createByteArray(100'000, [](int i) { return i * 11 + 5; }),
+	testDeletingHelper(createByteArray(100'000, [](int i) { return i * 11 + 5; }),
 					QVector<int>(50'000, 0));
 
 	// Delete the second half of a file
-	testDeleteBytes(createByteArray(100'000, [](int i) { return i * 11 + 5; }),
+	testDeletingHelper(createByteArray(100'000, [](int i) { return i * 11 + 5; }),
 					createVector<int>(50'000, [](int i) { return 100'000 - 1 - i; }));
 
 	// Delete 'random' bytes near the begining and near the end of the file
-	testDeleteBytes(createByteArray(1'000'000, [](int i) { return i * 9 + 3; }),
+	testDeletingHelper(createByteArray(1'000'000, [](int i) { return i * 9 + 3; }),
 					createVector<int>(10'000, [](int i) { return (i * 13 + 1) % (10'000); }) +
 					createVector<int>(10'000, [](int i) { return 960'000 + (i * 13 + 1) % (10'000); }));
 
 	// Delete 'random' bytes in the middle of the file
-	testDeleteBytes(createByteArray(1'000'000, [](int i) { return i * 17 + 3; }),
+	testDeletingHelper(createByteArray(1'000'000, [](int i) { return i * 17 + 3; }),
 					createVector<int>(10'000, [](int i) { return 500'000 + (i * 23 + 13) % (10'000); }));
 }
 
-void TestObject::testDeleteBytes(const QByteArray &data, const QVector<int> &indicesToDelete)
+void TestObject::testReadingHelper(const QByteArray &data, const QVector<int> &indicesToRead)
+{
+	QTemporaryFile file;
+	QVERIFY(file.open());
+	file.write(data);
+	{
+		BufferedEditor e(&file);
+		for (int index : indicesToRead) {
+			e.seek(index);
+			auto byte = e.getByte();
+			Q_ASSERT(byte.current);
+			Q_ASSERT(*byte.current == data[index]);
+		}
+	}
+}
+
+void TestObject::testDeletingHelper(const QByteArray &data, const QVector<int> &indicesToDelete)
 {
 	QByteArray expectedData = data;
 	for (int index : indicesToDelete)
